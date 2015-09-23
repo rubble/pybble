8a2a396c646046a4d45f9d445c1ccca801116a71
Emlyn Clay <eclay101@gmail.com>
2015-09-23 22:55:07 +0100

    Updates to match guide on http://peterdowns.com/posts/first-time-with-pypi.html

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup
 
 setup(name='pybble',
-      version='0.2',
+      packages=['pybble'],
+      version='0.1',
       description="""A python API to the Rubble programming
                      language over HTTP using the Rubble REST API.""",
-      url='http://github.com/emlync/pybble',
+      url='https://github.com/viditeck/pybble',
+      download_url='https://github.com/viditeck/pybble/tarball/0.1',
+      keywords = ['rubble', 'logic language', 'api'],
       author='Emlyn Clay',
       author_email='emlyn@viditeck.com',
       license='MIT',
-      packages=['pybble'],
       install_requires=["requests"],
       )
