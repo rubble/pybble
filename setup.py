03b7562656df3277f1559b33e2ec8967a7e57a68
Emlyn Clay <eclay101@gmail.com>
2015-09-24 14:58:11 +0100

    Nah, use explicit tag instead.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='pybble',
       description="""A python API to the Rubble programming
                      language over HTTP using the Rubble REST API.""",
       url='https://github.com/viditeck/pybble',
-      download_url='https://github.com/viditeck/pybble/tarball/master',
+      download_url='https://github.com/viditeck/pybble/tarball/0.1.1',
       keywords=['rubble', 'logic language', 'api'],
       author='Emlyn Clay',
       author_email='emlyn@viditeck.com',
