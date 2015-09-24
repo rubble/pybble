39076020d1ebffce96c6e6a311a65238c0e39861
Emlyn Clay <eclay101@gmail.com>
2015-09-24 14:56:36 +0100

    Bump version and change download url to pull from master.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(name='pybble',
       packages=['pybble'],
-      version='0.1',
+      version='0.1.1',
       description="""A python API to the Rubble programming
                      language over HTTP using the Rubble REST API.""",
       url='https://github.com/viditeck/pybble',
-      download_url='https://github.com/viditeck/pybble/tarball/0.1',
-      keywords = ['rubble', 'logic language', 'api'],
+      download_url='https://github.com/viditeck/pybble/tarball/master',
+      keywords=['rubble', 'logic language', 'api'],
       author='Emlyn Clay',
       author_email='emlyn@viditeck.com',
       license='MIT',
