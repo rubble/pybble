22129c5e99aeee86124ebcd218e8767d88765bce
Emlyn Clay <eclay101@gmail.com>
2016-09-29 22:43:43 +0100

    Update boilerplate to use new Client class

--- boilerplate.py
+++ boilerplate.py
@@ -1,16 +1,21 @@
 import os
-import pybble
-import requests
+from pybble.client import Client
 
 # Create a connection to Rubble using Pybble
 RUBBLE_API_KEY = os.environ.get('RUBBLE_API_KEY')
 RUBBLE_API_PASSWORD = os.environ.get('RUBBLE_API_PASSWORD')
+RUBBLE_SERVER_URL = os.environ.get('RUBBLE_SERVER_URL', "http://localhost:8082/")
+
 auth = (RUBBLE_API_KEY, RUBBLE_API_PASSWORD)
 
 config = {
-    "base_url": "http://localhost:8082/",
-    "verify_SSL": False,
+    "url": {
+        "base": RUBBLE_SERVER_URL,
+    },
+    "default_request_kwargs": {
+        "verify": False,
+    }
 }
 
-rubble = pybble.RubbleREST(auth=auth,
-                           config=config)
+rubble = Client(key=RUBBLE_API_KEY, password=RUBBLE_API_PASSWORD, config=config)
+
