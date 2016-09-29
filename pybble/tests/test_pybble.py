d23312402b4a2f91ba62976aed05d45a77511d46
Emlyn Clay <eclay101@gmail.com>
2016-09-29 22:43:01 +0100

    Rename TestProcess to TestAPI, all API methods need the same setUp and tearDown so it makes sense to group it. Add methods for testing file endpoints

--- pybble/tests/test_pybble.py
+++ pybble/tests/test_pybble.py
@@ -2,7 +2,10 @@ from unittest import TestCase
 import pybble
 
 
-class TestProcess(TestCase):
+class TestAPI(TestCase):
+    """
+    Tests the process API module, pybble.process
+    """
 
     def setUp(self):
         self.rubble = pybble.client.Client()
@@ -49,6 +52,33 @@ class TestProcess(TestCase):
         self.assertTrue(type(process_updated) is dict)
         self.assertFalse('error' in process_updated)
 
+    def test_processes_listed(self):
+        if len(self.processes) == 0:
+            process_created = self.create_a_process()
+
+        process_list = self.rubble.process.list()
+
+        self.assertTrue(type(process_list) is dict)
+        self.assertTrue('result' in process_list)
+
+    def test_file_created(self):
+        rubble_code = 'pybble_test;'
+        rubble_filename = 'pybble-test.rubble'
+        rubble_filename_path = '~system/private/{}'.format(rubble_filename)
+
+        file_created_successfully = self.rubble.file.put(rubble_filename_path, rubble_code)
+
+        self.assertTrue(file_created_successfully)
+
+    def test_file_can_be_got(self):
+        rubble_filename = 'fish'
+        rubble_filename_path = '~system/examples/rubblebox/{}'.format(rubble_filename)
+
+        file_data = self.rubble.file.get(rubble_filename_path)
+
+        self.assertTrue(type(file_data) is str, "Getting file should return string contents of the file")
+
     def tearDown(self):
+        # delete all left over processes
         for pid in self.processes:
             self.rubble.process.delete(pid)
